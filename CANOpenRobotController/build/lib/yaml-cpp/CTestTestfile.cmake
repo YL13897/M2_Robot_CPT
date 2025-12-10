96657ab8de945335053fe79414cbd14fd1f28904
YL <alexylin3@gmail.com>
2025-12-10 19:03:42 +1100

    git ignore /CANOpenRobotController/build/

--- CANOpenRobotController/build/lib/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yl/Desktop/M2_Robot_CPT/CANOpenRobotController/lib/yaml-cpp
-# Build directory: /home/yl/Desktop/M2_Robot_CPT/CANOpenRobotController/build/lib/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
